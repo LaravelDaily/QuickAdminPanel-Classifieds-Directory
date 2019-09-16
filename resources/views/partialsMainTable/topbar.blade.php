836c5b832386d39894d104b038096689bb65a7de
Laravel Daily <info@laraveldaily.com>
2019-09-16 13:32:57 +0300

    Update topbar.blade.php

--- resources/views/partialsMainTable/topbar.blade.php
+++ resources/views/partialsMainTable/topbar.blade.php
@@ -18,7 +18,7 @@
                                 </li>
                             @else
                                 <li class="dropdown">
-                                    <a href="/admin/home" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                    <a href="/admin/companies" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                         {{ Auth::user()->name }} <span class="caret"></span>
                                     </a>
 
